30b3be3b4633beec25d0c27980d17443f396f250
xiaotianxt <tianyp@pku.edu.cn>
2024-05-07 03:20:45 +0800

    feat: update

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,8 @@ export async function run(): Promise<void> {
     const feed = await new Parser().parseURL(url)
     const token =
       core.getInput('github_token') || (process.env.GITHUB_TOKEN as string)
-    const request = await new Promise<any>(r =>
-      r(import('@octokit/request') as any)
+    const request = await new Promise<any>(async r =>
+      r((await import('@octokit/request')).request)
     )
     const orequest = request.defaults({
       headers: {
