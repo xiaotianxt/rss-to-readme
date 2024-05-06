0a44c6e33c5fb8f143c1c6112eb5cf5d01d04059
xiaotianxt <tianyp@pku.edu.cn>
2024-05-07 03:11:37 +0800

    feat: update

--- src/main.ts
+++ src/main.ts
@@ -45,6 +45,7 @@ export async function run(): Promise<void> {
       sha: data.sha
     })
   } catch (error) {
+    console.log(error)
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
