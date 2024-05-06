2169448b34ab6b816e8a4f419401e7ab50810bef
xiaotianxt <tianyp@pku.edu.cn>
2024-05-07 02:42:28 +0800

    feat: update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import * as core from '@actions/core'
+import Parser from 'rss-parser'
 
+const parser = new Parser()
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
@@ -7,9 +9,9 @@ import * as core from '@actions/core'
 export async function run(): Promise<void> {
   try {
     const url = core.getInput('feed-url')
-    const res = await fetch(url)
-    const json = await res.json()
-    core.setOutput('feed', JSON.stringify(json))
+    const feed = await parser.parseURL(url)
+
+    console.log(feed)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
