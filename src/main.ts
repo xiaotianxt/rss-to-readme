f18eb6aa0ec6800570928e5a3558d59edec9d5bb
xiaotianxt <tianyp@pku.edu.cn>
2024-05-07 03:16:18 +0800

    feat: update

--- src/main.ts
+++ src/main.ts
@@ -12,44 +12,44 @@ export async function run(): Promise<void> {
     const token =
       core.getInput('github_token') || (process.env.GITHUB_TOKEN as string)
     console.log('HELLO')
-    const request = await new Promise<any>(r => import('@octokit/request'))
-    const orequest = request.defaults({
-      headers: {
-        authorization: `token ${token}`
-      }
-    })
-    const lines = feed.items
-      .slice(0, num)
-      .map(item => {
-        return '- [' + item.title + '](' + item.link + ')'
-      })
-      .join('\n')
-    console.log(lines)
+    // const request = await new Promise<any>(r => import('@octokit/request'))
+    // const orequest = request.defaults({
+    //   headers: {
+    //     authorization: `token ${token}`
+    //   }
+    // })
+    // const lines = feed.items
+    //   .slice(0, num)
+    //   .map(item => {
+    //     return '- [' + item.title + '](' + item.link + ')'
+    //   })
+    //   .join('\n')
+    // console.log(lines)
 
-    const owner = (process.env.GITHUB_REPOSITORY as string).split('/')[0]
-    const repo = (process.env.GITHUB_REPOSITORY as string).split('/')[1]
+    // const owner = (process.env.GITHUB_REPOSITORY as string).split('/')[0]
+    // const repo = (process.env.GITHUB_REPOSITORY as string).split('/')[1]
 
-    const { data } = await orequest('GET /repos/:owner/:repo/contents/:path', {
-      owner,
-      repo,
-      path: 'README.md'
-    })
+    // const { data } = await orequest('GET /repos/:owner/:repo/contents/:path', {
+    //   owner,
+    //   repo,
+    //   path: 'README.md'
+    // })
 
-    const content = Buffer.from(data.content, 'base64').toString('utf8')
-    console.log(content)
-    const newContent = content.replace(/\{FEED\}/, lines)
-    console.log(newContent)
+    // const content = Buffer.from(data.content, 'base64').toString('utf8')
+    // console.log(content)
+    // const newContent = content.replace(/\{FEED\}/, lines)
+    // console.log(newContent)
 
-    await orequest('PUT /repos/:owner/:repo/contents/:path', {
-      owner,
-      repo,
-      path: 'README.md',
-      message: 'Update README.md',
-      content: Buffer.from(newContent).toString('base64'),
-      sha: data.sha
-    })
+    // await orequest('PUT /repos/:owner/:repo/contents/:path', {
+    //   owner,
+    //   repo,
+    //   path: 'README.md',
+    //   message: 'Update README.md',
+    //   content: Buffer.from(newContent).toString('base64'),
+    //   sha: data.sha
+    // })
 
-    console.log(content, newContent)
+    // console.log(content, newContent)
   } catch (error) {
     console.log(error)
     // Fail the workflow run if an error occurs
