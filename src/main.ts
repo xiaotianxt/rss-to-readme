ebd55ea4302c5d50ba753e7a69333e8d22f3ac3c
xiaotianxt <tianyp@pku.edu.cn>
2024-05-07 02:36:48 +0800

    feat: update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
 
 /**
  * The main function for the action.
@@ -7,18 +6,9 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const url = core.getInput('feed-url')
+    const json = await fetch(url).then(response => response.json())
+    core.setOutput('feed', JSON.stringify(json))
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
